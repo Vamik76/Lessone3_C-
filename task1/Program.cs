660a8b46479961281c452d9f34c385a83d3f1197
Vamik76 <merts_v@mail.ru>
2022-12-06 13:51:30 +1000

    HW3C#_task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine ("Введите пятизначное чиcло");
+int a = Convert.ToInt32 (Console.ReadLine ());
+
+if (a>9999&&a<100000)
+{
+    Console.WriteLine ("пятизначное!!!");
+    string b = Convert.ToString(a);
+    if (b[0]==b[4]&&b[1]==b[3])
+        Console.Write ("палиндромом!!!");
+    else
+        Console.Write ("НО не палиндромом!!!"); 
+}
+else
+    Console.WriteLine("Введенное число не пятизначное!!!");
