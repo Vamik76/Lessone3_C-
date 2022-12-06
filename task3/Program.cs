7546c1d49d049de30c42a78f8d46d9d654d5042e
Vamik76 <merts_v@mail.ru>
2022-12-06 15:30:07 +1000

    HW3C#_task3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine ("Введите чиcло N");
+int n = Convert.ToInt32 (Console.ReadLine());
+if (n>0)
+{
+    int r=1;
+    for (int i=1;i<=n;i++)
+    {
+        r=i*i*i;
+        Console.Write(" "+r);
+    }
+}
+else
+    Console.WriteLine("Введено не натуральное чило");
